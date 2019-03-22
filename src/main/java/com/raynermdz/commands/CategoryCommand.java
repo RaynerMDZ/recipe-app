9143bd25776309f64ce26d4e74b154a6e7668a5e
Rayner Mendez <raynermendezg@gmail.com>
2019-03-22 16:04:23 -0400

    Converter classes added.

--- src/main/java/com/raynermdz/commands/CategoryCommand.java
+++ src/main/java/com/raynermdz/commands/CategoryCommand.java
@@ -1,4 +1,14 @@
 package com.raynermdz.commands;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class CategoryCommand {
+
+  private Long id;
+  private String description;
 }
---
+++ src/main/java/com/raynermdz/converters/CategoryToCategoryCommand.java
@@ -0,0 +1,4 @@
+package com.raynermdz.converters;
+
+public class CategoryToCategoryCommand {
+}
