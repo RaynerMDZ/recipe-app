9b1ecbd01409b45645f75bd2157230151ed3663b
Rayner Mendez <raynermendezg@gmail.com>
2019-03-25 15:32:20 -0400

    New recipe view added. new Recipe methods added.

--- src/main/java/com/raynermdz/controllers/RecipeController.java
+++ src/main/java/com/raynermdz/controllers/RecipeController.java
@@ -1,11 +1,12 @@
 package com.raynermdz.controllers;
 
-import com.raynermdz.models.Ingredient;
-import com.raynermdz.models.Recipe;
+import com.raynermdz.commands.RecipeCommand;
 import com.raynermdz.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,6 +24,20 @@ public class RecipeController {
     model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
     return "recipe/show";
+  }
 
+  @RequestMapping("recipe/new")
+  public String newRecipe(Model model) {
+    model.addAttribute("recipe", new RecipeCommand());
+    return "recipe/recipeform";
+  }
+
+  // Maps to th:action="@{/recipe/}"  method="post"
+  @PostMapping("recipe/")
+  public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+    RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+    // After saving the recipe, it redirects back to the recipe created.
+    return "redirect:/recipe/show/" + savedCommand.getId();
   }
 }
