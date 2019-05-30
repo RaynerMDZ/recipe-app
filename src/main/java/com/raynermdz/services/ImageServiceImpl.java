76dd6c6ae974c6bcf2e0c45a019129b4368f7cf4
Rayner Mendez <raynermendezg@gmail.com>
2019-05-30 16:59:34 -0400

    Image save and display implementation.

--- src/main/java/com/raynermdz/services/ImageServiceImpl.java
+++ src/main/java/com/raynermdz/services/ImageServiceImpl.java
@@ -1,16 +1,46 @@
 package com.raynermdz.services;
 
+import com.raynermdz.models.Recipe;
+import com.raynermdz.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
 
         log.debug("Received a file");
 
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+            Byte[] bytes = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()) {
+                bytes[i++] = b;
+            }
+            recipe.setImage(bytes);
+            recipeRepository.save(recipe);
+            log.debug("Saved to database.");
+
+        } catch (IOException e) {
+            log.error("Error occurred.", e);
+            e.printStackTrace();
+        }
     }
 }
