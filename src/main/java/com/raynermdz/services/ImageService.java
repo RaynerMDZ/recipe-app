c5a17feac7214e254e568a0005581ab8c5717598
Rayner Mendez <raynermendezg@gmail.com>
2019-05-30 16:38:37 -0400

    Picture Controller, service, serviceImpl added.

---
+++ src/main/java/com/raynermdz/services/ImageService.java
@@ -0,0 +1,8 @@
+package com.raynermdz.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
